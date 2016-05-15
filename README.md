b82b1c1bbcd159cfa22390b3535df658406d3709
Erik Helleren <erik.helleren@gmail.com>
2016-05-15 15:22:10 -0500

    Improved readme and logging

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # Convert videos to h265 (HEVC)
 
-So this is a simple ruby script that will chuch through a directory converting all of the videos to be h.265.
+So this is a simple ruby script that will chuch through a directory converting all of the videos to be h.265 in place.  This works by converting the video file to \*.tmp.mp4, and then moving it to its original file name with the mp4 extension.  It will delete the old version of the file.  
+
+
+## Usage
+
+This is a long running ruby script, it makes calls to FFMPEG using a ruby gem to scrape metadata of videos, and transcode them.  It works by appling some simple filters to create a queue of videos that can be converted, and then works through that queue.  
+
+|Constant| Description|
+|--|--|
+|DIRECTORY | the directory to recurse into.  All files will be considered within that directory. |
+|MIN_AGE_DAYS| How long agos does the ctime on a file have to be to be considered.  This is useful when using flexget to orgonize your movies/series and a torrent client might still be seeding the file.  |
+|VID_FORMATS| Possible file endings to consider.  The current value should sufice for most use cases |
+|LOG_LOCATION|  This Script is designed to run deetached in the background.  As such the log location is the best way to figure out whatis going on and the status of the conversion|  
+| PRESET | used to trade off between final file size and transcoding time.|
+
+
+## Logic
+
+1. Create a queue of videos that are at least MIN_AGE_DAYS old, a video file, and not encoded with h265 or HEVC already.
+2. Go through that queue one item at a time and transcode that file to HEVC, keeping the audio track in place.  (May remove embeeded subtitles)
+3. Probably never finish, because lets face it, you have a lot of movies
+
+
+## Picking a transoding preset
+
+These tests were run on a directory with 1 720p live action movie, 1 720p animated movie, 1 SD animated TV show, and 1 SD live action TV show.  The origional size of the directory 1.7 GB.  All videos were encoded with h264.
+
+Please pick the file compression level right for you, as there is a trade off between time and space.
+
+| preset | Conversion Duration | Final Size | Daily Freed sapce |
+|--|--|--|--|
+|ultrafast| | | |
+|veryfast| | | |
+|fast| | | |
+|slow| | | |
--- convertMovies.rb
+++ convertMovies.rb
@@ -8,16 +8,23 @@ require 'fileutils'
 require 'logger'
 
 DIRECTORY=  '/home/eh/git/convertTo265/test'
+
 # "/mnt/nasData/movies"
 MIN_AGE_DAYS=0
 VID_FORMATS = %w[.avi .flv .mkv .mov .mp4]
 LOG_LOCATION = "#{ENV['HOME']}/HevcConversion.log"
+PRESET='ultrafast'
 statusLogger=Logger.new(LOG_LOCATION)
 statusLogger.level=Logger::INFO
 
 
 def file_age(name)
   (Time.now - File.ctime(name))/(24*3600)
+end
+
+def seconds_to_s(total_time)
+  total_time=total_time.to_int
+  return [total_time / 3600, total_time/ 60 % 60, total_time % 60].map { |t| t.to_s.rjust(2,'0') }.join(':')
 end
 
 def get_aged_files(directory)
@@ -38,51 +45,40 @@ def get_aged_files(directory)
   return out
 end
 
-def get_candidate_file(possibileFiles)
-  possibileFiles.each do |file|
-    movie=FFMPEG::Movie.new(file)
-    if movie.valid? then
-      if movie.video_codec!="hevc" then
-        return file
-      else
-        puts "Found an already converted file: #{file}"
-      end
-    else
-      puts "File not a movie: #{file}"
-    end
-  end
-  return nil
-end
-
+# Returns a hash
 def get_candidate_files(possibileFiles)
-  out=[]
+  out={
+    movies: [],
+    runtime: 0
+  }
   possibileFiles.each do |file|
     movie=FFMPEG::Movie.new(file)
     if movie.valid? then
       if movie.video_codec!="hevc" then
-        out<< file
+        out[:movies]<< file
+        out[:runtime]=out[:runtime]+movie.duration
       end
     end
   end
+  out[:movies].sort!
   return out
 end
 
-def convert_file(filename)
-  video=FFMPEG::Movie.new(filename)
+def convert_file(video,filename)
   options={
     video_codec: 'libx265',
     threads: 3,
-    custom: "-preset slow -c:a copy"
+    custom: "-preset #{PRESET} -c:a copy"
     }
   outFileName=File.join(
     File.dirname(filename),
     "#{File.basename(filename,'.*')}")
-  puts outFileName
   out=video.transcode("#{outFileName}.tmp.mp4",options)
   FileUtils.mv("#{outFileName}.tmp.mp4","#{outFileName}.mp4")
   if filename!="#{outFileName}.mp4" then
     FileUtils.rm(filename)
   end
+  return out
 end
 
 possible_files=get_aged_files(DIRECTORY)
@@ -92,10 +88,24 @@ statusLogger.debug "Files to be checked: #{possible_files}"
 
 candidate_files= get_candidate_files(possible_files)
 
-statusLogger.info "There are a total of #{candidate_files.size} files that have not been converted yet."
-statusLogger.debug "Candidate Files that need to be re-encoded: #{possible_files}"
+statusLogger.info "There are a total of #{candidate_files[:movies].size} files that have not been converted yet."
+statusLogger.debug "Candidate Files that need to be re-encoded: #{candidate_files}"
+statusLogger.info "Total Duration: #{seconds_to_s(candidate_files[:runtime])}"
+remaining_runtime=candidate_files[:runtime]
+total_processing_time=0
+processed_video_duration=0
 
-candidate_files.each_with_index do |file,index|
-  statusLogger.info "Starting to transcode file #{index+1} of #{candidate_files.size}: #{file}"
-  convert_file(file)
+candidate_files[:movies].each_with_index do |file,index|
+  statusLogger.info "Starting to transcode file #{index+1} of #{candidate_files[:movies].size}: #{file}"
+  startTime=Time.now
+  video=FFMPEG::Movie.new(file)
+  converted_video=convert_file(video,file)
+  duration=Time.now - startTime
+  remaining_runtime-=video.duration
+  if !converted_video.nil? then
+    total_processing_time+=duration
+    processed_video_duration+=video.duration
+  end
+  avg=processed_video_duration/total_processing_time
+  statusLogger.info "Average videotime/walltime: #{avg}  Estimated time remaining #{seconds_to_s(remaining_runtime/avg)}"
 end
